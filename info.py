3825e5f253e119e917fab830b739fcf7c979e03e
OpManishG <145533753+OpManishG@users.noreply.github.com>
2023-12-03 09:24:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,9 +48,9 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "realme")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'upshrink.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '66418e89f79daa4610c64009aa67b72090ed710d')
+VERIFY = bool(environ.get('VERIFY', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'seturl.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'b727456575cb5ce5b438e987220f458882aa2bbb')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
